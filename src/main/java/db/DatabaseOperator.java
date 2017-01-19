ca5858238b8617389018e5037d993ce2fa20672b
ice1000 <ice1000kotlin@foxmail.com>
2017-01-20 00:14:59 +0800

    a little fix

--- src/main/java/db/DatabaseOperator.java
+++ src/main/java/db/DatabaseOperator.java
@@ -13,21 +13,32 @@ import java.util.HashSet;
  */
 public class DatabaseOperator {
 	public Article getArticle(int Id) throws SQLException {
-		ResultSet resultSet = Adapter.getAdapter().select("article", "*", new Pair("Id", "=" + Id));
+		ResultSet resultSet = Adapter.getAdapter().select("article", null, new Pair("Id", "=" + Id));
 		Article article;
 		String[] tags = resultSet.getString("tags").split(",");
 		HashSet<Tag> tags1 = new HashSet<>();
 		for (String thisTag : tags)
 			tags1.add(new Tag(thisTag));
 		ResultSet writerResultSet =
-				Adapter.getAdapter().select("writer", "*", new Pair("Id", "=" + resultSet.getInt("writerId")));
+				Adapter.getAdapter().select(
+						"writer",
+						null,
+						new Pair("Id", "=" + resultSet.getInt("writerId"))
+				);
 		Writer writer = new Writer(writerResultSet.getInt("Id"), writerResultSet.getString("name"),
 				writerResultSet.getString("motto"), writerResultSet.getURL("avasterURL"),
 				Genders.parseInt(writerResultSet.getInt("gender")));
 		for (String thisTag : tags)
 			tags1.add(new Tag(thisTag));
-		article = new Article(Id, resultSet.getInt("pdate"), writer, tags1, resultSet.getString("title"),
-				resultSet.getString("brief"), resultSet.getString("content"));
+		article = new Article(
+				Id,
+				resultSet.getInt("pdate"),
+				writer,
+				tags1,
+				resultSet.getString("title"),
+				resultSet.getString("brief"),
+				resultSet.getString("content")
+		);
 		return article;
 	}
 }
