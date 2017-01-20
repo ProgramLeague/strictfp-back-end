1f49147547e1b05f8106524db63fdc42c75bb695
ice1000 <ice1000kotlin@foxmail.com>
2017-01-21 00:06:00 +0800

    add test

---
+++ src/test/java/main/MainServerTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.junit.Test;
+
+/**
+ * Created by ice1000 on 2017/1/21.
+ */
+public class MainServerTest {
+	@Test
+	public void main() throws Exception {
+		MainServer.main(new String[]{});
+	}
+
+}
