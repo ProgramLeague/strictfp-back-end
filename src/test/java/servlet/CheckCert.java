c692ad06aea1888c1f68cb294492038f3299d01a
Eldath <lizhaohan001@live.cn>
2017-01-21 18:30:15 +0800

    懒得写测试= =

---
+++ src/test/java/servlet/CheckCert.java
@@ -0,0 +1,12 @@
+package servlet;
+
+/**
+ * Created by Eldath on 2017/1/21 0021.
+ *
+ * @author Eldath
+ */
+public class CheckCert {
+	public static void main(String[] args) {
+		//FIXME 懒得写测试了。你们自己看着我其他的测试写吧
+	}
+}
